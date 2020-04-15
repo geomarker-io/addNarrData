22f6a4c2bd967b2c3de8fb1ef087d1e48b0ef331
Erika <erika.rasnick@gmail.com>
2020-04-15 12:35:37 -0400

    create tibble in example

--- add_narr_data_example.R
+++ add_narr_data_example.R
@@ -1,10 +1,19 @@
-source('./narr_data_functions.R')
+library(tidyverse)
+source('./R/narr_data_functions.R')
 
-d <- d <- readRDS('/Users/RASV5G/OneDrive - cchmc/CF_local_data/d_narr_cells.rds') %>%
-  mutate(VisitDate = as.Date(VisitDate, format = '%m/%d/%y'), 
-         start_date = VisitDate - lubridate::days(7), 
-         end_date = VisitDate) %>% 
+d <- tibble::tribble(
+  ~id, ~VisitDate, ~narr_cell,
+  51981, "3/8/17", 56772,
+  77553, "2/6/12", 56772,
+  52284, "6/18/13", 57121,
+  96308, "2/25/19", 57121,
+  78054, "9/20/17", 56773
+) %>%
+  mutate(VisitDate = as.Date(VisitDate, format = '%m/%d/%y'),
+         start_date = VisitDate - lubridate::days(7),
+         end_date = VisitDate) %>%
   rename(narr_cell_number = narr_cell)
-  
 
 get_narr_data(d, narr_variables = c('air.2m', 'rhum.2m'))
+
+
